0e1fec9cc375f251c654532e929b0aaf97bb8556
keishiduki <zx400n6@gmail.com>
2026-03-31 15:34:12 +0900

    revert to original

---
+++ qmk_firmware/keyboards/yosuke2/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define EE_HANDS
+#define VIAL_KEYBOARD_UID {0xd7, 0xd0, 0xbc, 0x0b, 0xd3, 0xc3, 0xb1, 0x90}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
