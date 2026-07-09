153d0c4d982b8ac0a9328f7d49c95b96986b2ffd
keishiduki <zx400n6@gmail.com>
2026-07-09 13:04:14 +0900

    fix: Suspend復旧失敗の対策

--- qmk_firmware/keyboards/yosuke2/keymaps/vial/rules.mk
+++ qmk_firmware/keyboards/yosuke2/keymaps/vial/rules.mk
@@ -4,3 +4,9 @@ VIAL_INSECURE = yes
 
 # Override dynamic_keymap_reset
 LDFLAGS += -Wl,-wrap=dynamic_keymap_reset
+
+# PC起動時・復帰時の初期USBチェックをスキップ（RP2040で効果大）
+NO_USB_STARTUP_CHECK = yes
+
+# サスペンド時にマイコン自体を完全な低電力モードに移行させない
+NO_SUSPEND_POWER_DOWN = yes
